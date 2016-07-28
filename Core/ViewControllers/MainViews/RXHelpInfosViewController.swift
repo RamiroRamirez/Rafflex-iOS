889e2daf66acd26e7e7323e77707e8d1798d0bfe
Ramiro Ramirez <ramiro.ramirez@smartmobilefactory.com>
2016-07-28 21:43:23 +0200

    New views added for promos and Infos

---
+++ Core/ViewControllers/MainViews/RXHelpInfosViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RXHelpInfosViewController.swift
+//  Rafflex
+//
+//  Created by Ramiro Ramirez on 17/07/16.
+//  Copyright © 2016 RAM. All rights reserved.
+//
+
+import UIKit
+
+class RXHelpInfosViewController		: UIViewController {
+
+	@IBOutlet weak var tableView	: UITableView?
+
+	// MARK: - Actions
+
+	@IBAction func dismissViewController(sender: AnyObject) {
+		self.dismissViewControllerAnimated(true, completion: nil)
+	}
+}
+
+extension RXHelpInfosViewController: UITableViewDataSource, UITableViewDelegate {
+
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+}
